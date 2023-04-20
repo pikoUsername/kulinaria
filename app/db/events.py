72329cde5b73426a19aa4d7ed934774dd471eb16
pikoUsername <galymzhan.amantaj@gmail.com>
2023-04-21 00:56:32 +0600

    Fixed bug when creating permissions, groups
    docker compose now works fine

--- app/db/events.py
+++ app/db/events.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from loguru import logger
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 from app.core.settings.app import AppSettings
 from app.db.engine import Session, current_session
@@ -39,7 +39,8 @@ async def create_default_permissions(app: FastAPI) -> None:
 	logger.info("Creating default permissions...")
 	# horrible mess...
 	async with app.state.session() as session:
-		async with session.begin():
+		session: AsyncSession
+		try:
 			for perm in GlobalPermissions:
 				perm_mdl = PermissionInCreate(
 					name=perm.name,
@@ -48,6 +49,9 @@ async def create_default_permissions(app: FastAPI) -> None:
 				_, created = await PermissionsCrud.get_or_create(session, perm_mdl, id_name="name")
 				if created:
 					logger.info(f"Created permission: {perm.name}")
+		except Exception:
+			await session.rollback()
+			raise
 
 
 async def create_default_groups(app: FastAPI) -> None:
@@ -60,8 +64,11 @@ async def create_default_groups(app: FastAPI) -> None:
 	logger.info("Creating default groups...")
 
 	async with app.state.session() as session:
-		async with session.begin():
+		try:
 			for group in GlobalGroups:
 				gr, created = await GroupsCRUD.create_default(session, group)
 				if created:
 					logger.info(f"Group {group.name} created", extra={"groups": gr})
+		except Exception:
+			await session.rollback()
+			raise
