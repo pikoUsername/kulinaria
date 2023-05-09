a71928767250a83aee26cc537803d96ef259edcb
pikoUsername <galymzhan.amantaj@gmail.com>
2023-05-09 18:37:08 +0600

    Initial commit

---
+++ coolsite/foods/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
+
+from .models import *
+
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Контакты', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+]
+
+def index(request):
+    posts = Foods.objects.all()
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+    return render(request, 'foods/index.html', context=context)
+
+def about(request):
+    return render(request, 'foods/about.html',  {'menu': menu, 'title': 'О сайте'})
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id= {post_id}")
+
+def showcategory(request, cat_id):
+    return HttpResponse(f"Отображение категории с id= {cat_id}")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
